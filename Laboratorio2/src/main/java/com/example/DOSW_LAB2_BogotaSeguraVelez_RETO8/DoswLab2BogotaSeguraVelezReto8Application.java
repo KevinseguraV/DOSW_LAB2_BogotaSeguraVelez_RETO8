31bda594bd22e9230d031d5bdb0ef0dd61e2c108
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-15 15:39:59 -0500

    estructura con spring initializr

---
+++ Laboratorio2/src/main/java/com/example/DOSW_LAB2_BogotaSeguraVelez_RETO8/DoswLab2BogotaSeguraVelezReto8Application.java
@@ -0,0 +1,13 @@
+package com.example.DOSW_LAB2_BogotaSeguraVelez_RETO8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswLab2BogotaSeguraVelezReto8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswLab2BogotaSeguraVelezReto8Application.class, args);
+	}
+
+}
---
+++ Laboratorio2/src/test/java/com/example/DOSW_LAB2_BogotaSeguraVelez_RETO8/DoswLab2BogotaSeguraVelezReto8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DOSW_LAB2_BogotaSeguraVelez_RETO8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswLab2BogotaSeguraVelezReto8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
